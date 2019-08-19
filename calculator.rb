2093855db64c30d1a4ac4fe9eddb525ed2b170f2
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-19 00:37:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,22 @@
 # Add your variables here
+
+put first_number = 2
+
+put second_number = 4
+
+def sum 
+  first_number + second_number 
+end 
+
+def difference 
+  first_number - second number 
+end 
+
+def product 
+  first_number * second_number
+end 
+
+def quotent 
+  first_number # second_number
+end 
+
